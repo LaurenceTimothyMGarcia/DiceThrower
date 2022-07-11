fe3d2eeb9ea33d5bcb37675ded12af7e42dff0f7
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-07-10 18:38:50 -0700

    auto side checker

---
+++ Assets/Scripts/DiceSideCalc.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class DiceSideCalc : MonoBehaviour
+{
+    public Vector3[] diceNormals;
+    private MeshFilter filter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filter = GetComponent<MeshFilter>();
+        diceNormals = CalculateNormals(filter.mesh);
+    }
+
+    private Vector3[] CalculateNormals(Mesh mesh)
+    {
+        return mesh.normals.Distinct().ToArray();
+
+        //How to fix up smaller details
+
+        //Sort list of normals
+        //Take first value
+        //add to average
+        //Take next value
+        //Calc the dot product between average and next value
+        //If next value is < threshold
+        //add to average
+        //repeat from step 4
+        //else start new value and clear average to value
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/dice.cs
+++ Assets/Scripts/dice.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dice : MonoBehaviour
+public class Dice : MonoBehaviour
 {
     public int selectedResult;
     public int selectedVector;
