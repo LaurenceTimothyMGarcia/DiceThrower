d542e3b1743e1565e9c7f8947370368de28d3c03
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-07-11 22:34:34 -0700

    added comments to lay out goals

--- Assets/Scripts/d6Script.cs
+++ Assets/Scripts/d6Script.cs
@@ -7,6 +7,8 @@ public class d6Script : MonoBehaviour
     static Rigidbody rb;
     public static Vector3 diceVelocity;
 
+    private Transform[] listOfChecks;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,10 +18,11 @@ public class d6Script : MonoBehaviour
     {
         diceVelocity = rb.velocity;
 
-        if (Input.GetKeyDown (KeyCode.Space))
-        {
-            //DiceNumberTextScript.diceNumber = 0;
-            float dirX = Random.Range(0, 500);
-        }
+        //get dice to idle rotate center of screen if not selected
+
+        //user can click on die to move it and shake it
+            //Once user lets go throw dice
+
+        //dice stops moving and determines number
     }
 }
