be1d6e1c455ceab997b7846305ba712ca3437635
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-07-13 13:41:57 -0700

    dice is suspended in air then can be thrown

--- Assets/Scripts/d6Script.cs
+++ Assets/Scripts/d6Script.cs
@@ -8,21 +8,48 @@ public class d6Script : MonoBehaviour
     public static Vector3 diceVelocity;
 
     private Transform[] listOfChecks;
+    private bool isGrabbed = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void Update()
     {
+        Vector3 euler = transform.eulerAngles;
+
         diceVelocity = rb.velocity;
+        
 
         //get dice to idle rotate center of screen if not selected
+        if (!isGrabbed)
+        {
+            euler.x = Random.Range(0, 360f);
+            //euler.y = Random.Range(0f, 360f);
+            euler.z = Random.Range(0, 360f);
+            transform.eulerAngles = euler;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            isGrabbed = true;
+        }
 
         //user can click on die to move it and shake it
-            //Once user lets go throw dice
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //Place move and shake script here
+        }
 
-        //dice stops moving and determines number
+        //Once user lets go throw dice
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            rb.useGravity = true;
+        }
+
+        //dice stops moving and determines number 
+        //Have determine Number another script
     }
 }
