faf21bdeab94fb2dc7024ab64d92fc17d2c06a69
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-07-30 21:37:41 -0700

    text can work

--- Assets/Scripts/D6Check.cs
+++ Assets/Scripts/D6Check.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,7 @@ public class D6Check : MonoBehaviour
 
     [SerializeField] private DiceThrow diceThrow;
     [SerializeField] private Transform[] diceSides;
+    [SerializeField] TMP_Text diceResult;
 
     [HideInInspector] public Vector3 diceVelocity;
 
@@ -50,6 +52,6 @@ public class D6Check : MonoBehaviour
         }
 
         //Print result here
-        Debug.Log("Rolled: " + topSide);
+        diceResult.text = "Rolled: " + topSide;
     }
 }
