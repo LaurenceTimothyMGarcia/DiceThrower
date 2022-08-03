ddf193a5e1c143ef08d1bbdce40ed94322bacc07
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-08-03 12:07:28 -0700

    added cam follow dice

---
+++ Assets/Scripts/CamFollowDice.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*** Have Camera follow dice ***/
+
+public class CamFollowDice : MonoBehaviour
+{
+    [Header("Dice")]
+    [SerializeField] GameObject D4;
+    [SerializeField] GameObject D6;
+    [SerializeField] GameObject D8;
+    [SerializeField] GameObject D10;
+    [SerializeField] GameObject D12;
+    [SerializeField] GameObject D20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
